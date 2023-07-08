08b6953419ebe178f7788544f000c3cce462123b
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-07-08 06:11:52 +0100

    conditional statements exercise

--- test/test.js
+++ test/test.js
@@ -1,1 +1,9 @@
-console.log("test")
+const arr = []
+
+if (arr.length == 0) {
+  console.log("empty")
+} else if (arr.length < 4 && arr.length > 1) {
+  console.log("medium")
+} else {
+  console.log("large")
+}
