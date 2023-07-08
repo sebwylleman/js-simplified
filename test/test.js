22b7880e638587d4782575e675f69b63106d8ea9
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-07-08 06:41:13 +0100

    using for loops

--- test/test.js
+++ test/test.js
@@ -1,9 +1,4 @@
-const arr = []
-
-if (arr.length == 0) {
-  console.log("empty")
-} else if (arr.length < 4 && arr.length > 1) {
-  console.log("medium")
-} else {
-  console.log("large")
+for (let i = 0; i < 11; i++) {
+  if (i === 4) break
+  console.log(i)
 }
