22598114a4a1249da2228709149339071f3dcb4f
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-07-22 09:40:16 +0100

    refactored

--- test/test.js
+++ test/test.js
@@ -1,2 +1,13 @@
-let printHi = (name) => console.log(`Hi ${name}`)
-printHi("Seb")
+const items = [
+  { price: 10 },
+  { price: 100 },
+  { price: 3 },
+  { price: 12 },
+  { price: 3 },
+]
+
+const sum = items.reduce((acc, prix) => {
+  return acc + prix.price
+}, 0)
+
+console.log(sum)
