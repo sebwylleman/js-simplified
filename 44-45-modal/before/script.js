1ea69ebebf17e4f31a6231f7c80dcd571966eeae
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-07-25 14:37:39 +0100

    resetting exercise

--- 44-45-modal/before/script.js
+++ 44-45-modal/before/script.js
@@ -6,29 +6,11 @@
     * BONUS: overlay
 */
 
-const modal = document.querySelector("#modal")
-const openBtn = document.querySelector("#open-modal-btn")
-const closeBtn = document.querySelector("#close-modal-btn")
-const overlay = document.querySelector("#overlay")
-
 // TODO: 3. Create a click event listener for the open-modal-btn that adds the class
 //"open" to the modal
 // BONUS: Also add the class "open" to the overlay
 
-openBtn.addEventListener("click", () => {
-  modal.classList.add("open")
-  overlay.classList.add("open")
-})
-
 // TODO: 4. Create a click event listener for the close-modal-btn that removes the class "open" from the modal
 // BONUS: Also remove the class "open" from the overlay
 
-closeBtn.addEventListener("click", closeModal)
-
 // BONUS: Add a click event listener to the overlay that removes the class "open" from the modal and the overlay
-overlay.addEventListener("click", closeModal)
-
-function closeModal() {
-  modal.classList.remove("open")
-  overlay.classList.remove("open")
-}
