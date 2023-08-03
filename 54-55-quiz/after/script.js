5fba86decc34b75f121c6d290daea461830a209a
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-08-03 11:40:38 +0100

    exercise completed

--- 54-55-quiz/after/script.js
+++ 54-55-quiz/after/script.js
@@ -11,20 +11,20 @@ const questionItems = document.querySelectorAll(".question-item")
 const alert = document.querySelector("#alert")
 
 // TODO: 3. Create a submit event listener for the form that does the following.
-form.addEventListener("submit", e => {
+form.addEventListener("submit", (e) => {
   //    1. Prevent the default behaviour
   e.preventDefault()
 
   //    6. BONUS: Make sure unanswered questions show up as incorrect. The easiest way to do this is to add the incorrect class and removing the correct class from all question items before checking the correct answers
-  questionItems.forEach(questionItem => {
+  questionItems.forEach((questionItem) => {
     questionItem.classList.add("incorrect")
     questionItem.classList.remove("correct")
   })
 
   //    2. Get all selected answers (use the `checked` property on the input to determine if it is selected or not)
-  const checkedAnswers = answers.filter(answer => answer.checked)
+  const checkedAnswers = answers.filter((answer) => answer.checked)
   //    3. Loop through the selected answer to see if they are correct or not (Check the value of the answer to see if it is the string "true")
-  checkedAnswers.forEach(answer => {
+  checkedAnswers.forEach((answer) => {
     const isCorrect = answer.value === "true"
     const questionItem = answer.closest(".question-item")
 
@@ -39,7 +39,7 @@ form.addEventListener("submit", e => {
     }
     //    7. BONUS: If all answers are correct show the element with the id `alert` and hide it after one second (look into setTimeout) (use the class active to show the alert and remove the class to hide it)
 
-    const allTrue = checkedAnswers.every(answer => answer.value === "true")
+    const allTrue = checkedAnswers.every((answer) => answer.value === "true")
     const allAnswered = checkedAnswers.length === questionItems.length
 
     if (allTrue && allAnswered) {
--- 54-55-quiz/before/script.js
+++ 54-55-quiz/before/script.js
@@ -7,6 +7,8 @@
 */
 const form = document.getElementById("quiz-form")
 const answers = Array.from(document.querySelectorAll(".answer"))
+const questions = document.querySelectorAll(".question-item")
+const alert = document.getElementById("alert")
 
 // TODO: 3. Create a submit event listener for the form that does the following.
 //    1. Prevent the default behaviour
@@ -14,22 +16,39 @@ const answers = Array.from(document.querySelectorAll(".answer"))
 //    3. Loop through the selected answer to see if they are correct or not (Check the value of the answer to see if it is the string "true")
 //    4. For each correct answer add the class `correct` to the parent with the class `question-item` and remove the class `incorrect`.
 //    5. For each incorrect answer add the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
-//    6. BONUS: Make sure unanswered questions show up as incorrect. The easiest way to do this is to add the incorrect class and removing the correct class from all question items before checking the correct answers
+//
 //    7. BONUS: If all answers are correct show the element with the id `alert` and hide it after one second (look into setTimeout) (use the class active to show the alert and remove the class to hide it)
 
 form.addEventListener("submit", (e) => {
   e.preventDefault()
 
+  // // 6. BONUS: Make sure unanswered questions show up as incorrect.
+  // The easiest way to do this is to add the incorrect class and removing the correct class from all question items before checking the correct answers
+  questions.forEach((question) => {
+    question.classList.add("incorrect")
+    question.classList.remove("correct")
+  })
+
   const checkedAnswers = answers.filter((answer) => answer.checked)
 
   checkedAnswers.forEach((answer) => {
     const isCorrect = answer.value === "true"
-    const questionItem = answer.closest("question-item")
+    const questionItem = answer.closest(".question-item")
 
     if (isCorrect) {
       questionItem.classList.add("correct")
+      questionItem.classList.remove("incorrect")
     } else {
       questionItem.classList.add("incorrect")
+      questionItem.classList.remove("correct")
     }
   })
+
+  const allTrue = checkedAnswers.every((answer) => answer.value === "true")
+  if (allTrue) {
+    alert.classList.add("active")
+    setTimeout(() => {
+      alert.classList.remove("active")
+    }, 1000)
+  }
 })
